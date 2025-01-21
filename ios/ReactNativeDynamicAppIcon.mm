01d29ec5a41cc6f0cdfd67014d186d916aaac0e6
Anastasiia Nahaiska <anastasiia.nahaiska@computools.com>
2025-01-21 11:10:06 +0200

    feat: implemented ios module

--- ios/ReactNativeDynamicAppIcon.mm
+++ ios/ReactNativeDynamicAppIcon.mm
@@ -1,13 +1,17 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(ReactNativeDynamicAppIcon, NSObject)
+@interface RCT_EXTERN_MODULE (ReactNativeDynamicAppIcon, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getIcon
+                  : (RCTPromiseResolveBlock)resolve withRejecter
+                  : (RCTPromiseRejectBlock)reject)
 
-+ (BOOL)requiresMainQueueSetup
-{
+RCT_EXTERN_METHOD(changeIcon
+                  : (NSString *)iconName withResolver
+                  : (RCTPromiseResolveBlock)resolve withRejecter
+                  : (RCTPromiseRejectBlock)reject)
+
++ (BOOL)requiresMainQueueSetup {
   return NO;
 }
 
--- ios/ReactNativeDynamicAppIcon.swift
+++ ios/ReactNativeDynamicAppIcon.swift
@@ -1,8 +1,41 @@
 @objc(ReactNativeDynamicAppIcon)
 class ReactNativeDynamicAppIcon: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+  @objc(getIcon:withRejecter:)
+  func getIcon(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+    DispatchQueue.main.async {
+      if let currentIcon = UIApplication.shared.alternateIconName {
+        resolve(currentIcon)
+      } else {
+        resolve("DefaultIcon")
+      }
+    }
+  }
+
+  @objc(changeIcon:withResolver:withRejecter:)
+  func changeIcon(iconName: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+    DispatchQueue.main.async {
+      if !UIApplication.shared.supportsAlternateIcons {
+        reject("IOS:NOT_SUPPORTED", "Alternate icons are not supported on this device", nil)
+        return
+      }
+
+      let currentIcon = UIApplication.shared.alternateIconName
+
+      if iconName == currentIcon {
+        reject("IOS:ICON_ALREADY_USED", "The selected icon is already in use", nil)
+        return
+      }
+
+      let newIconName = iconName.isEmpty || iconName == "DefaultIcon" ? nil : iconName
+
+      UIApplication.shared.setAlternateIconName(newIconName) { error in
+        if let error = error {
+          reject("IOS:ICON_CHANGE_FAILED", "Failed to change icon: \(error.localizedDescription)", error)
+        } else {
+          resolve(newIconName ?? "DefaultIcon")
+        }
+      }
+    }
   }
 }
